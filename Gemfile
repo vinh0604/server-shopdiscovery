fca40ffe4e3c93dcd8db86cb77d52362db7e07ac
Vinh Bachsy <bsdvinhuit@gmail.com>
2012-11-25 21:25:53 +0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'pg'
+gem 'activerecord-postgis-adapter'
+gem 'grape'
+gem "squeel"
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem "haml-rails"
+
+# gems for testing
+group :development, :test do
+  gem "guard-rspec"
+  gem "rspec-rails"
+  gem "capybara"
+  gem 'rb-notifu'
+  gem 'guard-spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerShopdiscovery::Application.config.secret_token = 'f5bddc0a52cf917f9d9398d09e2739a18532a6875537f88d9107c43878fbffd532824422668402606c5329bb19161a412492b0e5901dfaa085351cf76d9a44ec'
